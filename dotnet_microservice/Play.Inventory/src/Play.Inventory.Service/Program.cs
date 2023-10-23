eb5874eb1acc9aef945f9e747acc0e3876130dd9
Minh giang trương <minhgiang241@gmail.com>
2023-10-23 15:55:00 +0700

    circurt breaker with Polly

--- dotnet_microservice/Play.Inventory/src/Play.Inventory.Service/Program.cs
+++ dotnet_microservice/Play.Inventory/src/Play.Inventory.Service/Program.cs
@@ -2,6 +2,8 @@ using Play.Common.Settings;
 using Play.Common.MongoDB;
 using Play.Inventory.Service.Entities;
 using Play.Inventory.Service.Clients;
+using Polly;
+using Polly.Timeout;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,11 +12,44 @@ var serviceSettings = builder.Configuration.GetSection(nameof(ServiceSettings)).
 Extensions.AddMongo(builder.Services)
           .AddMongoRepository<InventoryItem>("inventoryitems");
 
+Random jitterer = new Random();
+
 // Add services to the container.
 builder.Services.AddHttpClient<CatalogClient>(client =>
 {
     client.BaseAddress = new Uri("http://localhost:5048");
-});
+})
+.AddTransientHttpErrorPolicy(builders => builders.Or<TimeoutRejectedException>().WaitAndRetryAsync(
+      5,
+      retryApptempt => TimeSpan.FromSeconds(Math.Pow(2, retryApptempt)) + TimeSpan.FromMilliseconds(jitterer.Next(0, 1000)),
+      onRetry: (outcome, timespan, retryAttempt) =>
+      {
+          var serviceProvider = builder.Services.BuildServiceProvider();
+          serviceProvider.GetService<ILogger<CatalogClient>>()?
+          .LogWarning($"Delaying for {timespan.TotalSeconds} seconds, then making retry {retryAttempt}");
+      }
+
+      ))
+.AddTransientHttpErrorPolicy(builders => builders.Or<TimeoutRejectedException>().CircuitBreakerAsync(
+      3,
+      TimeSpan.FromSeconds(15),
+      onBreak: (outcome, timespan) =>
+      {
+          var serviceProvider = builder.Services.BuildServiceProvider();
+          serviceProvider.GetService<ILogger<CatalogClient>>()?
+          .LogWarning($"Openning the circuit for {timespan.TotalSeconds} seconds");
+
+      },
+      onReset: () =>
+      {
+          var serviceProvider = builder.Services.BuildServiceProvider();
+          serviceProvider.GetService<ILogger<CatalogClient>>()?
+          .LogWarning($"Closing the circuit");
+
+      }
+  ))
+.AddPolicyHandler(Policy.TimeoutAsync<HttpResponseMessage>(1));
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
